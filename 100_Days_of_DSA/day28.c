5293ad34258f00bb3992b6355b58fa5fe887c2ed
Daksh <its.dakshgoyal@gmail.com>
2026-03-01 07:46:21 +0530

    Add circular linked list implementation in day28.c
    Implement circular linked list creation and traversal with dynamic memory allocation.

---
+++ 100_Days_of_DSA/day28.c
@@ -0,0 +1,65 @@
+/* Circular Linked List Creation and Traversal - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the circular linked list elements starting from head, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    // Create circular linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Make it circular
+    temp->next = head;
+
+    // Traverse and print circular linked list
+    temp = head;
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+
+    return 0;
+}
